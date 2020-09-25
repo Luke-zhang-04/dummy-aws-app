d2567666d27c1ee165ee59ac1cf15447ad0a797e
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-09-25 12:28:37 -0400

    initialize serverless with aws lambda
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,25 @@
 import express from "express"
+import serverless from "serverless-http"
 
-const app = express(),
-    port = 3000
+const app = express(), // eslint-disable-next-line
+    port = process.env.PORT || 3000
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-app.listen(port, () => console.log(`Listening on: ${port}`))
+app.get("/api/info", (_, res) => {
+    res.send({
+        application: "sample-app",
+        version: "1.0",
+    })
+})
+
+app.post("/api/getback", (req, res) => {
+    res.send({...req.body})
+})
+
+if (process.argv.includes("--local")) {
+    app.listen(port, () => console.log(`Listening on: ${port}`))
+}
+
+export const handler = serverless(app)
